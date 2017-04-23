bbc986279e54f804e3ee46c10f933f00031d110c
LiviuSosu <sosuliviu@gmail.com>
2017-04-23 19:50:43 +0300

    Added C# Experiments and answers fom coursera
    Week 10 from coursers was very chanllenging to me so I find it necessary
    sharing it on github for other people references

---
+++ C# Experiments/MLTutorial/MLTutorial/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace MLTutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data Points:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Plot Line:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(191, 39);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(191, 85);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(412, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Plot Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(412, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Plot Line";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(88, 121);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 487);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ C# Experiments/MLTutorial/MLTutorial/Form1.cs
@@ -0,0 +1,126 @@
+﻿using MathNet.Numerics.LinearAlgebra.Double;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MLTutorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rand = new Random();
+        double[] xCoords;
+        double[] yCoords;
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            xCoords = new double[Convert.ToInt32(numericUpDown1.Value)];
+            yCoords = new double[Convert.ToInt32(numericUpDown1.Value)];
+
+            chart1.Titles.Clear();
+            chart1.Series.Clear();
+            chart1.Titles.Add("Linear Regression");
+
+            for (var i = 0; i < Math.Floor(xCoords.GetLength(0) / 2.0); i++)
+            {
+                xCoords[i] = rand.Next(i - xCoords.Length / 10, i + xCoords.Length / 10);
+            }
+
+            for (var i = Convert.ToInt32(Math.Floor(xCoords.GetLength(0) / 2.0)); i < xCoords.GetLength((0)); i++)
+            {
+                xCoords[i] = rand.Next(i + xCoords.Length / 5, i + xCoords.Length / 2);
+            }
+
+            for (var i = 0; i < Math.Floor(yCoords.GetLength(0) / 2.0); i++)
+            {
+                yCoords[i] = rand.Next(i, i + yCoords.Length / 5);
+            }
+
+            for (var i = Convert.ToInt32(Math.Floor(yCoords.GetLength(0) / 2.0)); i < yCoords.GetLength((0)); i++)
+            {
+                yCoords[i] = rand.Next(i - yCoords.Length / 10, i);
+            }
+
+            chart1.Series.Add("Data Points");
+            chart1.Series["Data Points"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+
+            for(var i = 0; i< xCoords.Length; i++)
+            {
+                chart1.Series["Data Points"].Points.AddXY(xCoords[i], yCoords[i]);
+            }
+
+            chart1.Series["Data Points"].Color = Color.DarkOrange;
+
+            chart1.Series.Add("QR Line");
+            chart1.Series["QR Line"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["QR Line"].Color = Color.DarkGreen;
+
+            chart1.Series.Add("Calc Line");
+            chart1.Series["Calc Line"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["Calc Line"].Color = Color.DarkRed;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chart1.Series["QR Line"].Points.Clear();
+            chart1.Series["Calc Line"].Points.Clear();
+
+            var degree = Convert.ToInt32(numericUpDown2.Value);
+
+            var X = new DenseMatrix(xCoords.Length, degree + 1);
+            X.SetColumn(0, DenseVector.Create(xCoords.Length, i=>1 ));
+
+            if(degree != 0)
+            {
+                X.SetColumn(1, xCoords);
+            }
+
+            for (int i = 2; i <= degree; i++)
+            {
+                X.SetColumn(i,X.Column(1).PointwiseMultiply(X.Column(i-1)));
+            }
+
+            var y = DenseMatrix.OfColumns(yCoords.Length,1,new[] { new DenseVector(yCoords) } );
+
+            var qrThetha = X.QR().Solve(y).ToColumnWiseArray();
+
+            //  //(XTX)-1 * (XTy).
+            var calcTheta = (X.Transpose().Multiply(X)).Inverse()
+                .Multiply(X.Transpose()).Multiply(y).ToColumnWiseArray();
+
+            var xMax = xCoords.Max();
+            var xMin = xCoords.Min();
+            var interval = (xMax - xMin) / Convert.ToDouble(numericUpDown1.Value);
+
+            for(var i=xMin; i<xMax;i+=interval)
+            {
+                chart1.Series["QR Line"].Points.AddXY(i, yPrediction(i, qrThetha));
+                chart1.Series["Calc Line"].Points.AddXY(i, yPrediction(i, calcTheta));
+            }
+        }
+
+        private static double yPrediction(double xPlot, double[] theta)
+        {
+            var yPlot = 0.0;
+
+            for (var i = 0; i < theta.Length; i++)
+            {
+                yPlot += theta[i] * Math.Pow(xPlot,i);
+            }
+
+            return yPlot;
+        }
+    }
+}
