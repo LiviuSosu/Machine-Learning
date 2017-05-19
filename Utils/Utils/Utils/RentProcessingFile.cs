f647ce8db0ecd9e41240b2cbeee90425c80c0e65
LiviuSosu <sosuliviu@gmail.com>
2017-05-19 14:42:44 +0300

    Tutorial with R

---
+++ Utils/Utils/Utils/RentProcessingFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Utils
+{
+    public class RentProcessingFile
+    {
+        string path = @"C:\Users\liviu.sosu\Work\Projects\Tutorial\Machine  Learning\Machine-Learning\Utils\Utils\Utils\";
+        public void Lines()
+        {
+            foreach (string line in File.ReadLines(path + "rent.txt"))
+            {
+                string[] info = line.Split(';');
+                using (StreamWriter sw = File.AppendText(path+"income.txt"))
+                {
+                    sw.WriteLine(info[0]);
+                }
+
+                using (StreamWriter sw = File.AppendText(path + "profit.txt"))
+                {
+                    sw.WriteLine(info[1]);
+                }
+            }
+        }
+    }
+
+}
